6176d5ab3a4a9336fd0c89d702dd2fd321e80a6d
Mr.E <31710172+mysteriouslymysterious@users.noreply.github.com>
2019-04-18 21:59:29 -0700

    Added Timer with controls & separate text control changer

--- app/src/main/java/com/example/deepfocustimer/MainActivity.java
+++ app/src/main/java/com/example/deepfocustimer/MainActivity.java
@@ -1,20 +1,72 @@
 package com.example.deepfocustimer;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Timer
+    private static final long START_TIME_IN_MILLIS = 600000;
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftMillis = START_TIME_IN_MILLIS;
+
+    // Other Button Function
+    private static final String TAG = "MainActivity";
+    private Button mButton;
+    private TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+
+        mButtonStartPause = findViewById(R.id.button_time_start);
+        mButtonReset = findViewById(R.id.button_time_reset);
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+
+        updateCountDownText();
+
+        //Other button functionality
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,7 +78,76 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+       mButton = findViewById(R.id.bottomButton);
+       mText = findViewById(R.id.middleBigText);
+
+       mButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Log.d(TAG, mText.getTextSize()+"onClick: welcome!");
+
+               if(mText.getTextSize() == 42){
+                   mButton.setTextColor(Color.BLUE);
+                   mButton.setBackgroundColor(Color.WHITE);
+                   mText.setTextColor(Color.MAGENTA);
+                   mText.setTextSize(30);
+               }
+               else if(mText.getTextSize() == 90){
+                   mButton.setTextColor(Color.BLACK);
+                   mButton.setBackgroundColor(Color.CYAN);
+                   mText.setTextColor(Color.BLACK);
+                   mText.setTextSize(14);
+               }
+
+           }
+       });
+
     }
+
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftMillis, 500) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+    private void resetTimer() {
+        mTimeLeftMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +170,10 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    /*public void lowerButtonClick(View view){
+        final String newString = new String();
+    }*/
 }
